c11543627c04d39ecd0591bfe5ab1867fe53df99
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-04-12 10:15:28 -0700

    Add emacasclient launcher

--- dotinstall.sh
+++ dotinstall.sh
@@ -29,3 +29,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+mkdir -p ~/.local/share/applications
+ln -s $dir/emacsclient.desktop ~/.local/share/applications
